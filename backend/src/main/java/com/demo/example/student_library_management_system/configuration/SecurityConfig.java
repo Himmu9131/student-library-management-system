41dc1f9fc7148e72ac1668a76ee690d25950dd10
Himmu9131 <hdwivedi96@gmail.com>
2025-08-05 14:07:59 +0530

    added cors url

--- backend/src/main/java/com/demo/example/student_library_management_system/configuration/SecurityConfig.java
+++ backend/src/main/java/com/demo/example/student_library_management_system/configuration/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500","https://library-frontend-bhmo.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Optional, only if using cookies or auth headers
