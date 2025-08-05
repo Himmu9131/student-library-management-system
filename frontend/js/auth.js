7873144e402df88c5d96401717fd90fa02339cac
Himmu9131 <hdwivedi96@gmail.com>
2025-08-05 17:41:31 +0530

    cofirming email password

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -134,6 +134,7 @@ async function login(event) {
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+  console.log(email+" "+password);
 
   try {
     const response = await fetch("https://student-library-management-system-g2wr.onrender.com/auth/login", {
